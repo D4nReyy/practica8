0319773e1e901206b25e56379050f1adab757afc
D4nReyy <123983205+D4nReyy@users.noreply.github.com>
2024-04-13 08:58:53 +0500

    Add files via upload

---
+++ practica8/practica8/practica8.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+struct Vehicle {
+    string number;
+    string brand;
+    string renter; 
+};
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    vector<Vehicle> vehicles = {
+        {"001", "Toyota", "Enterprise"},
+        {"002", "Ford", "Hertz"},
+        {"003", "Toyota", "Avis"},
+        {"004", "Chevrolet", "Enterprise"}
+    };
+
+    string desiredBrand = "Toyota"; 
+
+    map<string, vector<string>> carRenters;
+
+    for (const auto& vehicle : vehicles) {
+        if (vehicle.brand == desiredBrand) {
+            carRenters[vehicle.renter].push_back(vehicle.number);
+        }
+    }
+
+    cout << "Список легковых машин марки " << desiredBrand << ":\n";
+    for (const auto& pair : carRenters) {
+        cout << "Предприятие: " << pair.first << "\n";
+        cout << "Легковые машины: ";
+        for (const auto& carNumber : pair.second) {
+            cout << carNumber << " ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
